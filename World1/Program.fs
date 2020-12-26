9e770d906f176606930e4b7270955b2fcf4148a2
tk3 <tk3inc@gmail.com>
2020-12-26 20:57:07 +0900

    add TurnKeeper

--- World1/Program.fs
+++ World1/Program.fs
@@ -1,12 +1,25 @@
 ﻿open System
 
-type World() =
-  member _.Start(): unit =
-    printfn "start World ..."
+type TurnKeeper() =
+    let mutable turn = 0
+
+    member _.CurrentTrurn = turn
+
+    member _.OrderOccupants(): unit =
+        turn <- turn + 1
+        printfn "TurnKeeper[%d]> order occupants" turn
+
+type World(turnKeeper: TurnKeeper) =
+    let turnKeeper = turnKeeper
+
+    member _.Start(): unit =
+        printfn "start World ..."
+        turnKeeper.OrderOccupants()
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    let world = new World()
+    let turnKeeper = new TurnKeeper()
+    let world = new World(turnKeeper)
     world.Start()
     0
