925a40571c294a339e3fbd9868a528751ef49706
tk3 <tk3inc@gmail.com>
2020-12-26 20:15:59 +0900

    add World class

--- World1/Program.fs
+++ World1/Program.fs
@@ -1,6 +1,12 @@
 ﻿open System
 
+type World() =
+  member _.Start(): unit =
+    printfn "start World ..."
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let world = new World()
+    world.Start()
     0
